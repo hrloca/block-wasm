91edbd2cdcdb898e7ca95c2096ddae16514e8344
hrloca <hr.loca@gmail.com>
2022-05-25 17:37:29 +0900

    update

--- wasm/src/blocks/test.rs
+++ wasm/src/blocks/test.rs
@@ -3,15 +3,38 @@ mod tests {
     use super::super::*;
 
     #[test]
-    fn should_grouping() {
-        connect(to_blocks(vec![
-            vec![1, 2, 3, 4],
-            vec![1, 2, 3, 4],
-            vec![1, 2, 3, 4],
-            vec![1, 2, 3, 4],
+    fn should_do_next_handling() {
+        let blocks = to_blocks(vec![
+            vec![1, 1, 2, 2],
+            vec![2, 2, 1, 1],
+            vec![1, 3, 1, 1],
+            vec![1, 3, 1, 1],
+        ]);
+
+        let try_next = next_pos(&blocks);
+
+        assert_eq!(try_next((0, 0), Dir::Top), None);
+        assert_eq!(try_next((0, 0), Dir::Bottom), Some((1, 0)));
+        assert_eq!(try_next((0, 0), Dir::Right), Some((0, 1)));
+        assert_eq!(try_next((0, 0), Dir::Left), None);
+    }
+
+    #[test]
+    fn should_connecting() {
+        let conected = connect(&to_blocks(vec![
+            vec![1, 1, 2, 2],
+            vec![2, 2, 1, 1],
+            vec![1, 3, 1, 1],
+            vec![1, 3, 1, 1],
         ]));
 
-        assert!(true);
+        assert!(conected[0][0].connect.right);
+        assert!(conected[1][0].connect.right);
+        assert!(conected[1][1].connect.left);
+        assert!(conected[2][2].connect.top);
+        assert!(conected[2][2].connect.top);
+        assert!(conected[2][2].connect.bottom);
+        assert!(conected[2][2].connect.right);
     }
 
     #[test]
@@ -44,16 +67,9 @@ mod tests {
         assert_eq!(maynone, None);
     }
 
-    fn to_blocks(v: Vec<Vec<u8>>) -> Blocks {
-        v.iter()
-            .map(|row| {
-                row.iter()
-                    .map(|num| Block {
-                        kind: *num,
-                        group: None,
-                    })
-                    .collect()
-            })
+    fn to_blocks(v: Vec<Vec<u8>>) -> BlocksBody {
+        v.into_iter()
+            .map(|row| row.into_iter().map(Block::create).collect())
             .collect()
     }
 }
