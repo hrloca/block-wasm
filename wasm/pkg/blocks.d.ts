31c865eaf3260589d1ebfb0e623353b544d8deb9
hrloca <hr.loca@gmail.com>
2022-06-28 20:53:36 +0900

    update

--- wasm/pkg/blocks.d.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-/**
-* @returns {Promise<void>}
-*/
-export function run(): Promise<void>;
--- wasm/pkg/blocks.js
+++
@@ -1,3 +0,0 @@
-import * as wasm from "./blocks_bg.wasm";
-export * from "./blocks_bg.js";
-wasm.__wbindgen_start();
