bfb0d30bb22d2d7b4e923c7fc6a210f2d7e6c749
hrloca <hr.loca@gmail.com>
2022-06-29 09:47:33 +0900

    update

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./index.js",
   output: {
     path: path.resolve(__dirname, "docs"),
